0f16dd965f7a1a30db2081a2db18354efa8f0a73
dalba-de <D.alba85@gmail.com>
2020-04-26 11:20:42 +0200

    norme includes

--- includes/libasm.h
+++ includes/libasm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libasm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dalba-de <dalba-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/26 10:35:50 by marvin            #+#    #+#             */
-/*   Updated: 2020/04/26 10:35:50 by marvin           ###   ########.fr       */
+/*   Created: 2020/04/26 11:04:54 by dalba-de          #+#    #+#             */
+/*   Updated: 2020/04/26 11:20:01 by dalba-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,23 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
-# include <unistd.h>
 # include <fcntl.h>
+# include <unistd.h>
 
-size_t  ft_strlen(const char *s);
-int     ft_strcmp(const char *s1, const char *s2);
-char	*ft_strcpy(char *dst, const char *src);
-char    *ft_strdup(const char *s);
-int     ft_atoi_base(char *str, char *base);
-int	    ft_check_base(char c, char *base);
-int     ft_pow(int nbr, int base);
-ssize_t ft_write(int fd, const void *buf, size_t count);
-ssize_t ft_read(int fd, void *buf, size_t count);
+typedef struct		s_list
+{
+	void			*data;
+	struct s_list	*next;
+}					t_list;
+
+size_t				ft_strlen(const char *s);
+int					ft_strcmp(const char *s1, const char *s2);
+char				*ft_strcpy(char *dst, const char *src);
+char				*ft_strdup(const char *s);
+int					ft_atoi_base(char *str, char *base);
+int					ft_check_base(char c, char *base);
+int					ft_pow(int nbr, int base);
+ssize_t				ft_write(int fd, const void *buf, size_t count);
+ssize_t				ft_read(int fd, void *buf, size_t count);
 
 #endif
-
